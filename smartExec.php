3edcbd700ab2467d694ca83038c6dad48917aa27
Gökhan Çava <gokhancava@gmail.com>
2022-11-26 02:44:59 +0300

    Update smartExec.php

--- smartExec.php
+++ smartExec.php
@@ -1,6 +1,3 @@
 <?php
-exec('bash -c "exec nohup setsid php /home/admin/web/zz.com/public_html/calculate/test2.php '.$val.' > /dev/null 2>&1 &"');
-
-//$val is an option, you can send paramater with that. You dont have to use it like that
 
 exec('nohup setsid php /home/admin/web/zz.com/public_html/calculate/test2.php >> nohup.out 2>&1 &');
